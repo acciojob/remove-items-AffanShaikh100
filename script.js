d6a34182cfe683e735a26f9ecaeeb4890ae52166
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-31 18:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function remove(){
+let ele = document.getElementById("colorSelect");
+
+for(let i=0; i<ele.length;i++)
+	{
+		if(ele.value == ele[i]){
+			ele.remove[i];
+		}
+	}
+}
